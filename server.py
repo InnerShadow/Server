63d9dff2b473b2e2b998bfb8caa04a9d1b004cdb
Nobody <mishasikolenko@mail.ru>
2023-10-21 13:26:18 +0300

    Code clran

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
 import socket
 
 def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', 2000))
-    server.listen(4)
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 2000))
+        server.listen(4)
 
-    print('Working...')
-    client_socket, address = server.accept()
-    data = client_socket.recv(1024).decode('utf-8')
+        while True:
+            print('Working...')
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
 
-    print(data)
+            print(data)
 
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/htmp; charset=utf-8\r\n\r\n'
-    context = 'Well done, buddy...'.encode('utf-8')
+            HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/htmp; charset=utf-8\r\n\r\n'
+            context = 'Well done, buddy...'.encode('utf-8')
 
-    client_socket.send(HDRS.encode('utf-8') + context)
+            client_socket.send(HDRS.encode('utf-8') + context)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
 
-    print('shutsown this shit...')
+        print('shutsown this shit...')
 
+
+if __name__ == '__main__':
+    start_server()
+
+    
