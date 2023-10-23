7c1fb575a6b058dc9efff1f96216528c8ea10058
Nobody <mishasikolenko@mail.ru>
2023-10-23 18:33:44 +0300

    Make schedual table

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -6,16 +6,21 @@ class DataBase():
         with sq.connect("Data/billbiards.db") as con:
             self.cur = con.cursor()
 
-            self.cur = ("""CREATE TABLE IN FO EXIST roles (
-                        role_id INTEGER PRIMARY KEY AUTOINCRIMENT
-                        role_name TEXT)""")
+            self.cur.execute("""CREATE TABLE IN FO EXIST roles (
+                             role_id INTEGER PRIMARY KEY AUTOINCRIMENT,
+                             role_name TEXT
+                             )""")
 
             self.cur.execute("""CREATE TABLE IN FO EXIST users (
                              user_id INTEGER PRIMARY KEY AUTOINCRIMENT,
-                             login TEXT
-                             password_hash INTEGER
-                             FOREIGN KEY (role_id) REFERENCES roles(role_id))""")
+                             login TEXT,
+                             password_hash INTEGER,
+                             FOREIGN KEY (role_id) REFERENCES roles(role_id) NOT NULL DEFAULT 1
+                             )""")
             
-            
+            self.cur.execute("""CREATE TABLE IN FO EXIST schedual(
+                             schedual_id INTEGER PRIMARY KEY AUTOINCRIMENT,
+                             schedual_name TEXT 
+                             )""")
 
     pass
