4d17253d24a3dac57df4847732e224c7f81b9346
Nobody <mishasikolenko@mail.ru>
2023-10-23 18:51:28 +0300

    Create ad_schedual table

--- DataBase/DataBase.py
+++ DataBase/DataBase.py
@@ -13,9 +13,10 @@ class DataBase():
 
             self.cur.execute("""CREATE TABLE IN FO EXIST users (
                              user_id INTEGER PRIMARY KEY AUTOINCRIMENT,
+                             role_id INTEGER NOT NULL DEFAULT 1,
                              login TEXT,
                              password_hash INTEGER,
-                             FOREIGN KEY (role_id) REFERENCES roles(role_id) NOT NULL DEFAULT 1
+                             FOREIGN KEY (role_id) REFERENCES roles(role_id)
                              )""")
             
             self.cur.execute("""CREATE TABLE IN FO EXIST schedual(
@@ -26,15 +27,17 @@ class DataBase():
             self.cur.execute("""CREATE TABLE IN FO EXIST billboards_groop(
                              billboards_groop_id INTEGER PRIMARY KEY AUTOINCRIMENT,
                              groop_name TEXT,
-                             FOREIGN KEY (schedual_id) REFERENCES schedual(schedual_id) NOT NULL
+                             schedual_id INTEGER,
+                             FOREIGN KEY (schedual_id) REFERENCES schedual(schedual_id)
                              )""")
             
             self.cur.execute("""CREATE TABLE IN FO EXIST billboard(
                              billboard_id INTEGER PRIMARY KEY AUTOINCRIMENT,
+                             schedual_id INTEGER,
                              x_pos FLOAT NOT NULL,
                              y_pos FLOAT NOT NULL,
                              UNIQUE(x_pos, y_pos),
-                             FOREIGN KEY (schedual_id) REFERENCES schedual(schedual_id) NOT NULL
+                             FOREIGN KEY (schedual_id) REFERENCES schedual(schedual_id) 
                              )""")
             
             self.cur.execute("""CREATE TABLE IN FO EXIST ad(
@@ -42,5 +45,13 @@ class DataBase():
                              vidio_url TEXT NOT NULL,
                              ad_name TEXT
                              )""")
+            
+            self.cur.execute("""CREATE TABLE IN FO EXIST ad_schedual(
+                             schedual_id INTEGER,
+                             ad_id INTEGER,
+                             priority INTEGER NOT NULL,
+                             PRIMARY KEY(schedual_id, ad_id)
+                             FOREIGN KEY (schedual_id) REFERENCES schedual(schedual_id)
+                             FOREIGN KEY (ad_id) REFERENCES ad(ad_id))""")
 
     pass
